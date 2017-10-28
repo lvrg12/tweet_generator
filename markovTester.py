8b653ffaebcf5385a3995276d475171e500614fe
Lino Virgen <lino.virgen96@gmail.com>
2017-10-28 14:17:40 -0500

    markovTester wokring

--- markovTester.py
+++ markovTester.py
@@ -1,11 +1,38 @@
-from pymarkovchain import MarkovChain
+from markovchain import MarkovBase, MarkovSqliteMixin
+
+class Markov(MarkovSqliteMixin, MarkovBase):
+    pass
+
+markov = Markov(db='markov.db')
+
+with open('realDonaldTrump_tweets.csv') as fp:
+    markov.data(fp.read())
+
+with open('realDonaldTrump_tweets.csv') as fp:
+    for line in fp:
+        markov.data(line, True)
+markov.data('', False)
+
+print(*markov.generate(16, start=['sentence', 'start']))
+print(*markov.generate(16, start=['sentence', 'start']))
+print(*markov.generate(16, start=['sentence', 'start']))
+print(*markov.generate(16, start=['sentence', 'start']))
+print(*markov.generate(16, start=['sentence', 'start']))
+print(*markov.generate(16, start=['sentence', 'start']))
+print(*markov.generate(16, start=['sentence', 'start']))
+print(*markov.generate(16, start=['sentence', 'start']))
+print(*markov.generate(16, start=['sentence', 'start']))
+print(*markov.generate(16, start=['sentence', 'start']))
+print(*markov.generate(16, start=['sentence', 'start']))
+print(*markov.generate(16, start=['sentence', 'start']))
+print(*markov.generate(16, start=['sentence', 'start']))
+print(*markov.generate(16, start=['sentence', 'start']))
+print(*markov.generate(16, start=['sentence', 'start']))
+print(*markov.generate(16, start=['sentence', 'start']))
+print(*markov.generate(16, start=['sentence', 'start']))
+print(*markov.generate(16, start=['sentence', 'start']))
 
 
-# Create an instance of the markov chain. By default, it uses MarkovChain.py's location to
-# store and load its database files to. You probably want to give it another location, like so:
-mc = MarkovChain("./markov")
+markov.save()
 
-# To generate the markov chain's language model, in case it's not present
-mc.generateDatabase("This is a string of Text. It won't generate an interesting database though.")
-# To let the markov chain generate some text, execute
-mc.generateString()
+markov = Markov.load('markov.db')
